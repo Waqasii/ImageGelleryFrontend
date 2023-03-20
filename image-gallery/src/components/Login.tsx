aad1e814040f63b0141fd8ef68bb0852b34792de
waqasii <waqas199888@gmail.com>
2023-03-20 15:54:01 +0100

    setup graphql with react and develop initial login page

---
+++ image-gallery/src/components/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_MUTATION } from '../graphql/mutation';
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [login, { loading, error }] = useMutation(LOGIN_MUTATION);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            const { data } = await login({ variables: { username, password } });
+            localStorage.setItem('token', data.login.token);
+            // Redirect to dashboard or homepage
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <button type="submit" disabled={loading}>
+                    {loading ? 'Loading...' : 'Login'}
+                </button>
+                {error && <div>{error.message}</div>}
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ image-gallery/src/components/Profile.tsx
@@ -0,0 +1,7 @@
+export const Profile = () => {
+    return (
+        <div>
+            <h2>This is Profile page</h2>
+        </div>
+    )
+}
