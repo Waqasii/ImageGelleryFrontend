156ab67f86997f1bc9eb33ce74cdfaca48ec41dc
waqasii <waqas199888@gmail.com>
2023-03-22 21:49:56 +0100

    make a authorized request by including jwt token

--- image-gallery/src/index.tsx
+++ image-gallery/src/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, HttpLink } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { from, ApolloLink } from 'apollo-link';
+import { setContext } from '@apollo/client/link/context';
+
+
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-
 });
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
