2a0f7badc6f922f7b9045c411dd701b2ccaa2f33
Muhammad Waqas <waqas199888@gmail.com>
2023-03-24 14:26:39 +0100

    Delete config file for security risk

--- image-gallery/src/utils/AWSConfig.ts
+++
@@ -1,7 +0,0 @@
-export const s3Config = {
-    bucketName: 'resizeimgbuck',
-    region: 'eu-north-1',
-    accessKeyId: 'AKIA27FCXJXUQXZQB5HR',
-    secretAccessKey: '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',
-
-}
